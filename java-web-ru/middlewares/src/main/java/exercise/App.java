510991fe70c5b6e0eb1f1c2eeb3240da67301741
Alexey Kiselev <alexey4050@mail.ru>
2025-05-02 22:22:23 +0300

    Added solution task middlewares

---
+++ java-web-ru/middlewares/src/main/java/exercise/App.java
@@ -0,0 +1,59 @@
+package exercise;
+
+import io.javalin.Javalin;
+import exercise.controller.PostsController;
+import exercise.controller.RootController;
+import exercise.util.NamedRoutes;
+import io.javalin.rendering.template.JavalinJte;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get(NamedRoutes.rootPath(), RootController::index);
+
+        app.get(NamedRoutes.buildPostPath(), PostsController::build);
+        app.post(NamedRoutes.postsPath(), PostsController::create);
+
+        app.get(NamedRoutes.postsPath(), PostsController::index);
+        app.get(NamedRoutes.postPath("{id}"), PostsController::show);
+
+        app.get(NamedRoutes.editPostPath("{id}"), PostsController::edit);
+        app.post(NamedRoutes.postPath("{id}"), PostsController::update);
+
+        // BEGIN
+        app.after( ctx -> {
+            try {
+                byte[] responseBodyBytes = ctx.result().getBytes();
+                var digest = MessageDigest.getInstance("SHA-256");
+                byte[] hash = digest.digest(responseBodyBytes);
+                String hashString = Base64.getEncoder().encodeToString(hash);
+                ctx.header("X-Response-Degist", hashString);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
+
+
+
