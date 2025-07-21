323969343430396db654bb9989a45b508503da05
Alexey Kiselev <alexey4050@mail.ru>
2025-07-21 10:56:00 +0300

    Added solution task one-to-many

---
+++ java-spring-boot-ru/one-to-many/src/main/java/exercise/controller/TasksController.java
@@ -0,0 +1,109 @@
+package exercise.controller;
+
+import java.util.List;
+
+import exercise.dto.TaskCreateDTO;
+import exercise.dto.TaskDTO;
+import exercise.dto.TaskUpdateDTO;
+import exercise.mapper.TaskMapper;
+import exercise.mapper.UserMapper;
+import exercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.repository.TaskRepository;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/tasks")
+public class TasksController {
+    // BEGIN
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("")
+    public List<TaskDTO> index() {
+        var tasks = taskRepository.findAll();
+        return tasks.stream()
+                .map(p -> taskMapper.map(p))
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public TaskDTO show(@PathVariable Long id) {
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id "
+                        + id + " not found"));
+        return taskMapper.map(task);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskDTO create(@Valid @RequestBody TaskCreateDTO taskData) {
+        if (!userRepository.existsById(taskData.getAssigneeId())) {
+            throw new ResourceNotFoundException("User with id "
+                    + taskData.getAssigneeId() + " not found");
+        }
+
+        if (taskRepository.findByTitle(taskData.getTitle()).isPresent()) {
+            throw new ResourceNotFoundException("Task with title "
+                    + taskData.getTitle() + " already exists");
+        }
+
+        var task = taskMapper.map(taskData);
+        taskRepository.save(task);
+        return taskMapper.map(task);
+    }
+
+    @PutMapping("/{id}")
+    public TaskDTO update(@PathVariable Long id, @Valid @RequestBody TaskUpdateDTO taskData) {
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + id
+                        + "not found"));
+
+        if (!userRepository.existsById(taskData.getAssigneeId())) {
+            throw new ResourceNotFoundException("User with id " + taskData.getAssigneeId()
+                    + "not found");
+        }
+        if (!taskData.getTitle().equals(task.getTitle()) &&
+                taskRepository.findByTitle(taskData.getTitle()).isPresent()) {
+            throw new ResourceNotFoundException("Task with title " + taskData.getTitle()
+                    + "already exists");
+        }
+
+        taskMapper.update(taskData, task);
+        var updateTask = taskRepository.save(task);
+        return taskMapper.map(updateTask);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Task with id " + id
+                    + " not found");
+        }
+        taskRepository.deleteById(id);
+    }
+
+    // END
+}
